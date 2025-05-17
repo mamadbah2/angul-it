3c1ce845dedba344a2919a58b5fc83ba35844326
mamadbah2 <bahmamadoubobosewa@gmail.com>
2025-05-17 18:20:13 +0000

    change level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgIf} from '@angular/common';
+import {CaptchaLevelComponent} from './captcha-level/captcha-level.component';
+import {CaptchaComponent} from './captcha/captcha.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, CaptchaLevelComponent, CaptchaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
